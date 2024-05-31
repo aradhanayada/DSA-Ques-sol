ca45c3119203199e1e5f8828de2ace8c5024708e
Aradhana Yadav <103102710+aradhanayada@users.noreply.github.com>
2024-05-31 08:36:13 +0530

    Create Two pointer.js

---
+++ Arrays and Two pointer/Two pointer.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target)
+ {
+    let n=nums.length;
+    for(let i=0;i<=n-2;i++)
+     {
+            for(let j=i+1;j<n;j++) 
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    return [i,j];
+                }
+            }
+      
+     }
+    
+};
