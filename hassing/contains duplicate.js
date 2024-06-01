baee5b3a033332bf5f2cc783f925fd05fa401993
Aradhana Yadav <103102710+aradhanayada@users.noreply.github.com>
2024-06-01 11:53:22 +0530

    Create contains duplicate.js

---
+++ hassing/contains duplicate.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let hashMap = {};
+
+    for (let i = 0; i <nums.length; i++) {
+        if (hashMap[nums[i]]) {
+            return true;
+        }
+        else{
+        hashMap[nums[i]] = 1;
+        }
+    }
+    return false;
+};
